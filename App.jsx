05b7f26443cd3f5898c47b08cf80f21f6cdf3379
simon0820s <simon0820s@gmail.com>
2023-05-21 11:19:51 -0500

    add styles

--- App.jsx
+++ App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       .then((data) => {
         const {fact}=data
         setFact(fact);
-        const splitFirstWords=fact.split(" ",5).join(" ")
+        const splitFirstWords=fact.split(" ",8).join(" ")
 
         fetch(`https://cataas.com/cat/says/${splitFirstWords}?json=true`)
         .then(response => response.json())
@@ -31,9 +31,14 @@ const App = () => {
 
   return (
     <main>
-      <h1>{fact ? fact : "loading"}</h1>
+      <div className="text">
+        <h1>Random phrase</h1>
+        <h2>{fact ? fact : "loading"}</h2>
+      </div>
+      <div className="content">
       {img && <img src={img} alt="img from cataas.com"/>}
-      <button onClick={handleFact}>Click me</button>
+      <button onClick={handleFact}>new Cat</button>
+      </div>
     </main>
   );
 };
